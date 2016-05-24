866871ad4e69bd97e0a3d3648d657133df99151e
631410038 <631410038@SALA503PC16.fspoaeduc.com.br>
2016-05-24 11:31:17 -0300

    Versão 1.0 - Entidades

---
+++ 100Organic-DB/src/java/model/Produto.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 631410038
+ */
+@Entity
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String nomeProduto;
+    private String qualidadeProduto;
+    private int quantidadeProduto;
+    private double precoProduto;
+    private String medidaProduto;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getQualidadeProduto() {
+        return qualidadeProduto;
+    }
+
+    public void setQualidadeProduto(String qualidadeProduto) {
+        this.qualidadeProduto = qualidadeProduto;
+    }
+
+    public int getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public String getMedidaProduto() {
+        return medidaProduto;
+    }
+
+    public void setMedidaProduto(String medidaProduto) {
+        this.medidaProduto = medidaProduto;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Produto)) {
+            return false;
+        }
+        Produto other = (Produto) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Produto[ id=" + id + " ]";
+    }
+    
+}
